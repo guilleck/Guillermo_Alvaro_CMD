55c3609d3f8945953f9764ca426a6f663e87082c
Guillermo <gcecaillan@gmail.com>
2024-03-24 13:30:03 +0100

    Modificaciones Guillermo

---
+++ src/com/calculadora/Calculator.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author luism
+ */
+/**
+ * Clase Calculator que proporciona operaciones matemáticas básicas.
+ */
+public class Calculator {
+    
+    /**
+     * Suma dos números enteros.
+     * @param a Primer número entero.
+     * @param b Segundo número entero.
+     * @return La suma de a y b.
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Resta dos números enteros.
+     * @param a Primer número entero.
+     * @param b Segundo número entero.
+     * @return La diferencia entre a y b.
+     */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Métodos para multiplicar y dividir con comentarios Javadoc similares...
+}
+
+
